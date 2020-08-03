40ac7d23586c35de10bf87dc904d96d3c17a5b78
Chandler Hanson <chan.hanson5@gmail.com>
2020-08-03 22:19:27 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,9 +4,9 @@ require "pry"
 def apply_coupons(cart, coupons)
   coupons.each do |coupon_item|
     #check the coupon against each item in the cart
-    binding.pry
     cart.each do |cart_item|
       #check whether it's the same item.
+      binding.pry
       if cart_item[:item] == coupon_item[:item]
         if cart_item[:count] >= coupon_item[:num]
           cart_item[:count] = cart_item[:count] - coupon_item[:num]
