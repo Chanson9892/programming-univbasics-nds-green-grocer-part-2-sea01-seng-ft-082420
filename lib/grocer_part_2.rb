0cba0419d1dd9bcf7a2d69624dcc826148d8fe73
Chandler Hanson <chan.hanson5@gmail.com>
2020-08-03 22:05:37 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,43 +1,23 @@
 require_relative './part_1_solution.rb'
 
-
 def apply_coupons(cart, coupons)
-  coupons.each do |coupon|
-    coupon.each do |attribute, value|
-      name = coupon[:item]
-      if cart[name] && cart[name][:count] >= coupon[:num]
-        if cart["#{name} W/COUPON"]
-          cart["#{name} W/COUPON"][:count] += 1
-        else
-          cart["#{name} W/COUPON"]  = {:price => coupon[:cost], :clearance => cart[name][:clearance], :count => 1}
-        end
-      cart[name][:count] -= coupon[:num]
-      end
-    end
-  end
-  cart
+
 end
 
 def apply_clearance(cart)
-  discount = 0.80
-  cart.each do |item, details|
-    if details[:clearance] == true
-      details[:price] = (details[:price]*discount).round(2)
-    end
-  end
-  cart
+  # Consult README for inputs and outputs
+  #
+  # REMEMBER: This method **should** update cart
 end
 
 def checkout(cart, coupons)
-  total = 0
-  new_cart = consolidate_cart(cart)
-  coupon_cart = apply_coupons(new_cart, coupons)
-  clearance_cart = apply_clearance(coupon_cart)
-  clearance_cart.each do |item, attribute_hash|
-      total += (attribute_hash[:price] * attribute_hash[:count])
-    end
-  if total > 100
-    total = (total * 0.9)
-  end
-  total
+  # Consult README for inputs and outputs
+  #
+  # This method should call
+  # * consolidate_cart
+  # * apply_coupons
+  # * apply_clearance
+  #
+  # BEFORE it begins the work of calculating the total (or else you might have
+  # some irritated customers
 end
